810db64a9b04a05cfff2d59733be6f29a13cc993
sergey-master-new <prodvizh.com@gmail.com>
2022-01-12 16:43:14 +0300

    Initial commit

---
+++ src/main/java/com/example/task03app/Task03appApplication.java
@@ -0,0 +1,13 @@
+package com.example.task03app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task03appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task03appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task03app/Task03appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task03app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task03appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
